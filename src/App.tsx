932ba489d89584a7fc36d9e141f9983b094775ae
David <51981848+gcdavid@users.noreply.github.com>
2022-06-21 12:58:06 +0545

    changed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-3xl'>
+      I am app
+    </div>
+  )
+}
+
+export default App
